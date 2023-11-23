02608eda69e8a74f44aa0f1a468de97ebe6b5569
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-23 12:47:43 +0530

    Added:Length Computation method

--- LineComparisonBuilder.java
+++ LineComparisonBuilder.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 
 class LineComparisonBuilder {
+    public static double lengthComputation(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
     public static void main(String[] args) {
-       System.out.println("Welcome to Line Comparison Computation");
+        System.out.println("Welcome to Line Comparison Computation");
+        
+        // getting input of points from user 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the first Point X-cordinate: ");
+        double firstxcord = sc.nextDouble();
+        System.out.println("Please Enter the first Point Y-cordinate : ");
+        double firstycord = sc.nextDouble();
+        System.out.println("Please Enter the second Point X-cordinate : ");
+        double secondxcord = sc.nextDouble();
+        System.out.println("Please Enter the second Point Y-cordinate : ");
+        double secondycord = sc.nextDouble();
+
+        // invoking length computation method on the given points by user 
+        System.out.println("The total length of the line formed by the Points (" + firstxcord +"," +firstycord+
+        " ) and ( " +secondxcord + "," + secondycord+ ") is :" + lengthComputation(firstxcord,firstycord,secondxcord,secondycord));
     }
 }
