99d53f3bc40a7fb3d0ecd82286696707ed9da59a
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-23 14:59:22 +0530

    Refactored: Implemented Equality and Comparison member function of class

--- LineComparisonBuilder.java
+++ LineComparisonBuilder.java
@@ -8,33 +8,21 @@ class LineComparisonBuilder {
         Line L1 = new Line("Line1",1, 2, 3, 4);
         Line L2 = new Line("Line2", 6, 9, 12, 15);
 
-        // invoking lengthComparison method
-        double length1 = L1.lengthComputation();
-        double length2 = L2.lengthComputation();
+       
+        
 
-        // checking equality of two lines
+        // calling equality method of line L1 with Line L2 as its parameter
+        L1.equality(L2); 
 
-        String str1 = Double.toString(length1);
-        String str2 = Double.toString(length2);
-
-        System.out.println(str1.equals(str2)); // true or false;
-
-        /**
-         * comparing two lines
-         * if str1 == str2 return 0
-         * if str1>str2 return +ve
-         * if str1<str2 return -ve
-         **/
-        System.out.println(str1.compareTo(str2));
-        System.out.println(str2.compareTo(str1));
-
-      
+        // calling comparison method of line L1 with Line L2 as its parameter
+      L1.comparison(L2);
     }
 }
 
 class Line {
     String name;
     int x1, x2, y1, y2;
+    double length;
 
     Line(String name, int x1, int y1, int x2, int y2) {
         this.name = name;
@@ -42,6 +30,7 @@ class Line {
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
+        this.length = lengthComputation();  // invoking lengthComparison method in its constructor only 
     }
 
     Line(Line line) {
@@ -50,6 +39,7 @@ class Line {
         this.x2 = line.x2;
         this.y1 = line.y1;
         this.y2 = line.y2;
+        this.length = line.length;
     }
 
     double lengthComputation() {
@@ -57,6 +47,30 @@ class Line {
         System.out.println("The total length of the " + this.name + " formed by the Points (" + x1 + "," + y1 +
                 " ) and ( " + x2 + "," + y2 + ") is :" + length);
         return length;
+    }
+ 
+
+   
+    // making equality as member function and other line required for equalsto() is obtained from function argument
+    void equality(Line L1) {
+        String str1 = Double.toString(length);
+        String str2 = Double.toString(L1.length);
+        boolean flag = str1.equals(str2); // true or false
+        if (flag)
+            System.out.println(this.name + " is equal to " + L1.name);
+        else
+            System.out.println(this.name + " is not equal to " + L1.name);
+
+    }
+    
+    //making comparison as member function and other line required for compareTo() is obtained from fnc arg
+    void comparison(Line L1) {
+         int flag = Double.compare(this.length, L1.length);
+         if (flag == 0)
+             System.out.println(this.name + " is equal to " + L1.name);
+         else if (flag > 0)
+             System.out.println(this.name + " is greater than " + L1.name);
+        else System.out.println(this.name +" is less than "+ L1.name);
     }
 
    
