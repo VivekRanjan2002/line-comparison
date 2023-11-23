8bc42c3a3835e3d8e560ff874a21be9fcdf1c699
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-23 14:28:17 +0530

    Refactored: Introduction of Class

--- LineComparisonBuilder.java
+++ LineComparisonBuilder.java
@@ -1,49 +1,63 @@
 import java.util.Scanner;
 
 class LineComparisonBuilder {
-
-    // length computation method on the given points by user 
-    public static double lengthComputation(String line) {
-        
-         // getting input of points from user 
-         Scanner sc = new Scanner(System.in);
-         String literal = " is : ";  
-        System.out.println("Please Enter the first Point X-cordinate of "+ line + literal);
-        double x1 = sc.nextDouble();
-        System.out.println("Please Enter the first Point Y-cordinate of " + line +literal);
-        double y1 = sc.nextDouble();
-        System.out.println("Please Enter the second Point X-cordinate : " + line +literal);
-        double x2 = sc.nextDouble();
-        System.out.println("Please Enter the second Point Y-cordinate : " + line + literal);
-        double y2 = sc.nextDouble();
-
-        double length= Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("The total length of the "+ line+ " formed by the Points (" + x1 +"," +y1+
-                " ) and ( " + x2 + "," + y2 + ") is :" + length);
-        return length;
-    }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation");
-        
-       
-        // invoking length computation method
-        double length1 = lengthComputation("line 1");
-        double length2 = lengthComputation("line 2");
-       
-        // checking equality of two lines 
-     
+
+        // creating two lines with respective cordinates
+        Line L1 = new Line("Line1",1, 2, 3, 4);
+        Line L2 = new Line("Line2", 6, 9, 12, 15);
+
+        // invoking lengthComparison method
+        double length1 = L1.lengthComputation();
+        double length2 = L2.lengthComputation();
+
+        // checking equality of two lines
+
         String str1 = Double.toString(length1);
         String str2 = Double.toString(length2);
 
         System.out.println(str1.equals(str2)); // true or false;
 
-         
-        /**comparing two lines
-        if str1 == str2 return 0
-        if str1>str2 return +ve
-        if str1<str2 return -ve
-        **/
-        System.out.println(str1.compareTo(str2)); 
+        /**
+         * comparing two lines
+         * if str1 == str2 return 0
+         * if str1>str2 return +ve
+         * if str1<str2 return -ve
+         **/
+        System.out.println(str1.compareTo(str2));
         System.out.println(str2.compareTo(str1));
+
+      
     }
+}
+
+class Line {
+    String name;
+    int x1, x2, y1, y2;
+
+    Line(String name, int x1, int y1, int x2, int y2) {
+        this.name = name;
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    Line(Line line) {
+        this.name = line.name;
+        this.x1 = line.x1;
+        this.x2 = line.x2;
+        this.y1 = line.y1;
+        this.y2 = line.y2;
+    }
+
+    double lengthComputation() {
+        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("The total length of the " + this.name + " formed by the Points (" + x1 + "," + y1 +
+                " ) and ( " + x2 + "," + y2 + ") is :" + length);
+        return length;
+    }
+
+   
 }
